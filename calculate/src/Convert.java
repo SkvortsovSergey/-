d1783fdc4226abe5ee45979ad378a89e6933ce30
Sergey Skvortsov <skvortzovsergey@gmail.com>
2021-09-11 21:21:14 +0300

    second commit

--- calculate/src/Convert.java
+++ calculate/src/Convert.java
@@ -2,7 +2,7 @@ public class Convert {
     private final String[] ROMNUM = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
     private int intNum;
 
-    public Integer convertStr(String romNum) {
+    public Integer convertStr(String romNum) {//перевод римских цифр в арабские
         for (int i = 0; i < 11; i++) {
         if (romNum.equals(ROMNUM[i])){  intNum = i;}
         }
@@ -10,7 +10,7 @@ public class Convert {
         return intNum ;
     }
 
-    public String convertInt(int num) {
+    public String convertInt(int num) {//перевод арабских цифр в римские
         String rom = num >= 0 ? ROMNUM[num] : "Ошибка";
         return rom;
     }
