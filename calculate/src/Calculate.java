47f6c7b1dd1a4bda953de71a098195c933999523
Sergey Skvortsov <skvortzovsergey@gmail.com>
2021-09-18 17:23:27 +0300

    final edition

--- calculate/src/Calculate.java
+++ calculate/src/Calculate.java
@@ -2,17 +2,19 @@ public class Calculate {
     private int result;
     public Calculate(char ch, String first, String second) {
         Convert convert = new Convert(first, second);
+        int firstInt = convert.getIntNum();
+        int secondInt = convert.getIntNum2();
         switch (ch) {
-            case '+' -> result = (convert.getIntNum() + convert.getIntNum2());
-            case '-' -> result = (convert.getIntNum() - convert.getIntNum2());
-            case '*' -> result = (convert.getIntNum() * convert.getIntNum2());
-            case '/' -> result = (convert.getIntNum() / convert.getIntNum2());
+            case '+' -> result = (firstInt + secondInt);
+            case '-' -> result = (firstInt - secondInt);
+            case '*' -> result = (firstInt * secondInt);
+            case '/' -> result = (firstInt / secondInt);
         }
         if (!Character.isDigit(first.charAt(0)) && !Character.isDigit(second.charAt(0)) && result > 0) {
             System.out.println(convert.convertInt(result));
         } else if (!Character.isDigit(first.charAt(0)) && !Character.isDigit(second.charAt(0)) && result <= 0) {
             System.out.println("Недопустимое значение");
-        } else if(Character.isDigit(first.charAt(0)) && Character.isDigit(second.charAt(0))&&convert.getIntNum()<11&&convert.getIntNum()>0&&convert.getIntNum2()<11&&convert.getIntNum2()>0){
+        } else if(Character.isDigit(first.charAt(0)) && Character.isDigit(second.charAt(0))&&firstInt<11&&firstInt>0&&secondInt<11&&secondInt>0){
             System.out.println(result);
         }
     }
