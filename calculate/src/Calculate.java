82afc521d9540297f4e54d5de8190b768171e265
Sergey Skvortsov <skvortzovsergey@gmail.com>
2021-09-18 17:08:39 +0300

    final edition

--- calculate/src/Calculate.java
+++ calculate/src/Calculate.java
@@ -12,7 +12,7 @@ public class Calculate {
             System.out.println(convert.convertInt(result));
         } else if (!Character.isDigit(first.charAt(0)) && !Character.isDigit(second.charAt(0)) && result <= 0) {
             System.out.println("Недопустимое значение");
-        } else {
+        } else if(Character.isDigit(first.charAt(0)) && Character.isDigit(second.charAt(0))&&convert.getIntNum()<11&&convert.getIntNum()>0&&convert.getIntNum2()<11&&convert.getIntNum2()>0){
             System.out.println(result);
         }
     }
